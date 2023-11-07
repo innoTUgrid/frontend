ccb9ea00db07230516e22d43868b044bf7808dd7
Tobias Loch <Tobias.loch@mail.de>
2023-11-07 12:17:09 +0100

    moved navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './navigation/navbar.component';
+import { NavbarComponent } from './components/navigation/navbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
