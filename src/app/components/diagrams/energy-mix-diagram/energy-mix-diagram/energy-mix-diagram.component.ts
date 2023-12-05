c8f90db900230568ef251bb7d63fd992b020ce87
Tobias loch <Tobias.loch@mail.de>
2023-12-05 13:52:20 +0100

    moved rxjs functionality of energy consumption to kpi-service implemented functionality for energy-mix diagram

--- src/app/components/diagrams/energy-mix-diagram/energy-mix-diagram/energy-mix-diagram.component.ts
+++ src/app/components/diagrams/energy-mix-diagram/energy-mix-diagram/energy-mix-diagram.component.ts
@@ -2,10 +2,11 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Props } from 'src/app/types/props';
 import { KpiService } from 'src/app/services/kpi.service';
 
-import * as Highcharts from 'highcharts';
+import * as Highcharts from 'highcharts/highstock';
 import HighchartsExporting from 'highcharts/modules/exporting';
 import HighchartsData from 'highcharts/modules/data';
 import HighchartsAccessibility from 'highcharts/modules/accessibility';
+import { HighchartsDiagram, KPI, SeriesTypes } from 'src/app/types/kpi.model';
 
 
 
@@ -18,19 +19,28 @@ HighchartsAccessibility(Highcharts);
   templateUrl: './energy-mix-diagram.component.html',
   styleUrls: ['./energy-mix-diagram.component.scss']
 })
-export class EnergyMixDiagramComponent implements OnInit{
+export class EnergyMixDiagramComponent implements OnInit, HighchartsDiagram {
   @Input() props: Props = { value: [10, 20, 30] };
   kpiService: KpiService;
-  
+
   green = '';
   yellow = '';
   orange = '';
   color3 = '';
   color4 = '';
+
+  chart: Highcharts.Chart | undefined;
+  colors: string[] = [this.green, this.yellow, this.orange, this.color3, this.color4];
+  seriesType: SeriesTypes = 'area';
   
   constructor(kpiService: KpiService) {
     this.kpiService = kpiService;
   }
+
+  set updateFlag(value: boolean) {
+    if (value) this.chart?.update(this.chartProperties, true)
+  }
+  get updateFlag(): boolean { return false};
 
   ngOnInit() {
     this.green = getComputedStyle(document.documentElement).getPropertyValue('--highcharts-color-0').trim();
@@ -39,106 +49,123 @@ export class EnergyMixDiagramComponent implements OnInit{
     this.color3 = getComputedStyle(document.documentElement).getPropertyValue('--highcharts-color-3').trim();
     this.color4 = getComputedStyle(document.documentElement).getPropertyValue('--highcharts-color-4').trim();
     this.initChart();
+
+    this.kpiService.subscribeEnergyDiagram(this, KPI.ENERGY_CONSUMPTION);
   }
+
+  dataGrouping: Highcharts.DataGroupingOptionsObject = {
+    approximation: 'sum',
+    enabled: true,
+    forced: true,
+    units: [['day', [1]]]
+  }
+
+  xAxis: Highcharts.XAxisOptions = {
+    id: 'xAxis',
+    type: 'datetime',
+    dateTimeLabelFormats: {
+      minute: '%H:%M',
+    },
+  }
+
+  chartProperties: Highcharts.Options = {
+    chart: {
+      type: 'area',
+      renderTo: 'energyMixChart',
+    },
+    title: {
+      text: 'Energy-mix',
+      align: 'center',
+    },
+    credits: {enabled: false},
+
+    xAxis: this.xAxis,
+    yAxis: {
+      title: {
+        text: 'CO2 Emissions',
+      },
+    },
+    plotOptions: {
+      area: {
+        dataGrouping: this.dataGrouping,
+      }
+    },
+
+    series: [
+      {
+        name: 'Total',
+        type: 'area', 
+        color: this.green,
+        marker:{
+          lineColor: this.green,
+        },
+        data: [
+          [new Date("2019-01-02T00:00:00.000Z"),38000 + 22534],
+          [new Date("2019-01-02T01:00:00.000Z"),37300 + 23599],
+          // 37892 + 24533,
+          // 38564 + 25195,
+          // 36770 + 25896,
+          // 36026 + 27635,
+          // 34978 + 29173,
+          // 35657 + 32646,
+          // 35620 + 35686,
+          // 35971 + 37709,
+          // 36409 + 39143,
+          // 36435 + 36829,    
+      ]
+      },
+      {
+        name: 'Scope 1',
+        type: 'area', 
+        color: this.yellow,
+        marker:{
+          lineColor: this.yellow,
+        },
+        data: [
+          [new Date("2019-01-02T00:00:00.000Z"),38000],
+          [new Date("2019-01-02T01:00:00.000Z"),37300],
+          // 37892,
+          // 38564,
+          // 36770,
+          // 36026,
+          // 34978,
+          // 35657,
+          // 35620,
+          // 35971,
+          // 36409,
+          // 36435,
+      ],
+      },
+      {
+        name: 'Scope 2',
+        type: 'area',
+        color: this.orange,
+        marker:{
+          lineColor: this.orange,
+        },
+        data: [
+              [new Date("2019-01-02T00:00:00.000Z"), 22534],
+              // plus an hour
+              [new Date("2019-01-02T01:00:00.000Z") , 23599],
+              // 24533,
+              // 25195,
+              // 25896,
+              // 27635,
+              // 29173,
+              // 32646,
+              // 35686,
+              // 37709,
+              // 39143,
+              // 36829,
+          ],
+      },
+    ],
+  };
 
   private initChart() {
-   
-    
-
-    
-    Highcharts.chart({
-      chart: {
-        type: 'area',
-        renderTo: 'energyMixChart',
-      },
-      title: {
-        text: 'Energy-mix',
-        align: 'center',
-      },
-
-      xAxis: {
-        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-      },
-      yAxis: {
-        title: {
-          text: 'CO2 Emissions',
-        },
-      },
-      //colors: [this.green, this.yellow, this.orange],
-
-      series: [
-        {
-          name: 'Total',
-          type: 'area', 
-          color: this.green,
-          marker:{
-            lineColor: this.green,
-          },
-          data: [
-            38000 + 22534,
-            37300 + 23599,
-            37892 + 24533,
-            38564 + 25195,
-            36770 + 25896,
-            36026 + 27635,
-            34978 + 29173,
-            35657 + 32646,
-            35620 + 35686,
-            35971 + 37709,
-            36409 + 39143,
-            36435 + 36829,    
-        ]
-        },
-        {
-          name: 'Scope 1',
-          type: 'area', 
-          color: this.yellow,
-          marker:{
-            lineColor: this.yellow,
-          },
-          data: [
-            38000,
-            37300,
-            37892,
-            38564,
-            36770,
-            36026,
-            34978,
-            35657,
-            35620,
-            35971,
-            36409,
-            36435,
-        ],
-        },
-        {
-          name: 'Scope 2',
-          type: 'area',
-          color: this.orange,
-          marker:{
-            lineColor: this.orange,
-          },
-          data: [
-                22534,
-                23599,
-                24533,
-                25195,
-                25896,
-                27635,
-                29173,
-                32646,
-                35686,
-                37709,
-                39143,
-                36829,
-            ],
-        },
-      ],
-
-      
-
-    });
+    this.chart = Highcharts.chart(this.chartProperties);
   }
+  
   
   
 
--- src/app/services/kpi.service.ts
+++ src/app/services/kpi.service.ts
@@ -4,6 +4,7 @@ import { BehaviorSubject, Observable, combineLatest, map } from 'rxjs';
 import * as testData from './data/readKPIs.json';
 
 import { TimeSeriesDataPoint, TimeSeriesDataDictionary, TimeInterval } from '../types/time-series-data.model';
+import { HighchartsDiagram, KPI as KPI } from '../types/kpi.model';
 
 @Injectable({
   providedIn: 'root'
@@ -32,29 +33,99 @@ export class KpiService {
     this.loadTimeSeriesData();
   }
 
+  // energy subscription
+  subscribeEnergyDiagram(diagram: HighchartsDiagram, kpiName: KPI, updateTimeInterval: boolean = true) {
+    this.timeSeriesData$$.subscribe((data:TimeSeriesDataDictionary) => {
+      const energy = data.get(kpiName)
+      if (!energy) {
+        return
+      }
+
+      const series: Array<Highcharts.SeriesOptionsType> = []
+      const energyTypes = new Set(energy.map(entry => entry.meta.type))
+      for (const type of energyTypes) {
+        const typeData = energy.filter(entry => entry.meta.type === type)
+        const color = (diagram.colors.length > series.length) ? diagram.colors[series.length] : undefined
+        series.push({
+          name: type,
+          id: type, 
+          data:typeData.map(entry => ([entry.time.getTime(), entry.value])),
+          type: diagram.seriesType,
+          color: color,
+          marker:{
+            lineColor: color,
+          },
+        })
+      }
+      if (diagram.chart) {
+        diagram.chart.update({
+          series: series
+        })
+      } else { // this is only reachable for code that uses highcharts-angular
+        diagram.chartProperties.series = series
+        diagram.updateFlag = true
+      }
+    });
+
+    this.timeInterval$$.subscribe((timeInterval) => {
+      if (diagram.chart) {
+        diagram.chart.xAxis[0].setExtremes(timeInterval.start.getTime(), timeInterval.end.getTime(), false);
+        diagram.dataGrouping.units = [[timeInterval.stepUnit, [timeInterval.step]]]
+        diagram.chart.axes[0].setDataGrouping(diagram.dataGrouping, true)
+      } else { // this is only reachable for code that uses highcharts-angular
+        diagram.xAxis.min = timeInterval.start.getTime();
+        diagram.xAxis.max = timeInterval.end.getTime();
+        diagram.dataGrouping.units = [[timeInterval.stepUnit, [timeInterval.step]]]
+        diagram.updateFlag = true
+      }
+    });
+  }
+
   loadTimeSeriesData():void {
-    const key = 'energyConsumption'
-    const value = testData[key];
-    const convertedData = value.map(entry => ({time: new Date(entry.time), value: entry.value, meta: entry.meta }));
-    this.timeSeriesData.set(key, convertedData);
+    const keys: Array<KPI> = [KPI.ENERGY_CONSUMPTION, KPI.AUTARKY]
+    for (const key of keys) {
+      const value = testData[key];
+      const convertedData = value.map(entry => ({time: new Date(entry.time), value: entry.value, meta: entry.meta }));
+      this.timeSeriesData.set(key, convertedData);
+    }
     this.timeSeriesData$$.next(this.timeSeriesData);
-    // const interval = this.timeInterval$$
-    // setTimeout(function() {
-    //   interval.next({
-    //     start:new Date("2019-01-01T01:00:00.000Z"),
-    //     end:new Date("2019-01-01T02:00:00.000Z"),
-    //     step:15,
-    //     stepUnit:"minute"
-    //   });
-    // }, 5000);
-    // setTimeout(function() {
-    //   interval.next({
-    //     start:new Date("2019-01-01T02:00:00.000Z"),
-    //     end:new Date("2019-01-01T03:00:00.000Z"),
-    //     step:30,
-    //     stepUnit:"minute"
-    //   });
-    // }, 10000);
+    const interval = this.timeInterval$$
+    const timeSeriesDataSubject = this.timeSeriesData$$
+    const timeSeriesData = this.timeSeriesData
+    setTimeout(function() {
+      // interval.next({
+      //   start:new Date("2019-01-01T01:00:00.000Z"),
+      //   end:new Date("2019-01-01T02:00:00.000Z"),
+      //   step:15,
+      //   stepUnit:"minute"
+      // });
+      const map = new Map<string, TimeSeriesDataPoint[]>();
+      for (const key of keys) {
+        const data = timeSeriesData.get(key);
+        if (data) {
+          map.set(key, data.slice(0,data.length/2));
+        }
+      }
+      timeSeriesDataSubject.next(map);
+      console.log(map)
+    }, 2500);
+    setTimeout(function() {
+      // interval.next({
+      //   start:new Date("2019-01-01T02:00:00.000Z"),
+      //   end:new Date("2019-01-01T03:00:00.000Z"),
+      //   step:30,
+      //   stepUnit:"minute"
+      // });
+      const map = new Map<string, TimeSeriesDataPoint[]>();
+      for (const key of keys) {
+        const data = timeSeriesData.get(key);
+        if (data) {
+          map.set(key, data.slice(data.length/2, data.length));
+        }
+      }
+      timeSeriesDataSubject.next(map);
+      console.log(map)
+    }, 5000);
   }
 
 }
