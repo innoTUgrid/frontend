8455635d578037cd51ff83bb80b0f33081872ff3
Nastassia Lukyanovich <n.lukyanovich@tu-berlin.de>
2023-12-16 00:03:17 +0100

    implemented 3 out of 4 diagramms for the comparison view, command bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { OverviewComponent } from './views/overview/overview.component';
 import { DatasetsComponent } from './views/datasets/datasets/datasets.component';
 import { InfoviewComponent } from './views/infoview/infoview.component';
 import { EnergyFlowViewComponent } from './views/energy-flow-view/energy-flow-view.component';
+import { ComparisonViewComponent } from './views/comparison-view/comparison-view.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/overview', pathMatch: 'full' },
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: 'datasets', component: DatasetsComponent },
   { path: 'info', component:InfoviewComponent },
   { path: 'energyflow', component: EnergyFlowViewComponent },
+  { path: 'comparison', component: ComparisonViewComponent},
  
 ];
 
