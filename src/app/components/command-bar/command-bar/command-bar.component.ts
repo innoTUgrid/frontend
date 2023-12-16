6cb70fa062b5ab86282936cd0e6a5ccd0a38d168
Thomas Janke <janke@tu-berlin.de>
2023-12-16 22:08:14 +0100

    add feature to automatically choose granularity to sane values

--- src/app/components/command-bar/command-bar/command-bar.component.ts
+++ src/app/components/command-bar/command-bar/command-bar.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { KpiService } from 'src/app/services/kpi.service';
 import {Granularity} from 'src/app/types/granularity.model'
 import { TimeInterval, TimeUnit } from 'src/app/types/time-series-data.model';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-command-bar',
@@ -14,27 +15,41 @@ export class CommandBarComponent {
   kpiService: KpiService = inject(KpiService)
 
   granularities = Object.values(Granularity);
+  granularityInHours: {[k: string]: number} = { 'hour': 1, 'day': 24, 'week': 24 * 7, 'month': 24 * 30, 'quarter': 24 * 30 * 3, 'year': 24 * 365};
   
   selectedGranularity: string = '';
 
   timeInterval!: TimeInterval;
   selectedView?: string | null;
 
-  recentPeriods: {[k: string]: number} = { 'today': 0, 'last7Days': 7, 'last31Days': 31, 'lastYear': 365};
+  recentPeriods: {[k: string]: number} = { 'today': 0, 'last 7 days': 7, 'last 31 days': 31, 'last year': 365};
+  recentPeriodNames: string[] = Object.entries(this.recentPeriods).sort((a, b) => a[1] - b[1]).map(([key, value]) => key);
   recentPeriodToDisplay = '';
+  granularityType: 'select' | 'button' = 'button';
 
   applyFilters(singleDate?: boolean) {
+    const diff = this.timeInterval.end.diff(this.timeInterval.start, 'days');
+    // make sure granularity is sane
+    const granularity = Object.entries(this.granularityInHours).find(([key, entry]) => {
+      return (diff * 24 / entry) < 50;
+    })?.[0] || Granularity.HOUR;
+
+    this.selectedGranularity = granularity;
+    // set recent period
+    this.recentPeriodToDisplay = '';
+    if (this.timeInterval.end.isSame(moment().endOf('day'))) {
+      this.recentPeriodToDisplay = Object.entries(this.recentPeriods).find(([key, value]) => value == diff)?.[0] || '';
+    }
+    this.setTimeInterval(singleDate);
+  }
+
+
+  setTimeInterval(singleDate?: boolean) {
     this.timeInterval = {
       start: this.timeInterval.start.clone().startOf('day'), 
       end: singleDate ? this.timeInterval.start.clone().endOf('day') : this.timeInterval.end.clone().endOf('day'),
       step: (this.selectedGranularity == Granularity.QUARTER) ? 3 : 1,
       stepUnit: (this.selectedGranularity == Granularity.QUARTER) ? Granularity.MONTH : this.selectedGranularity as TimeUnit
-    }
-    // set recent period
-    this.recentPeriodToDisplay = '';
-    if (this.timeInterval.end.isSame(moment().endOf('day'))) {
-      const diff = this.timeInterval.end.diff(this.timeInterval.start, 'days');
-      this.recentPeriodToDisplay = Object.entries(this.recentPeriods).find(([key, value]) => value == diff)?.[0] || '';
     }
     this.kpiService.timeInterval$$.next(this.timeInterval);
   }
@@ -57,6 +72,7 @@ export class CommandBarComponent {
     let start = moment().startOf('day').subtract(this.recentPeriods[this.recentPeriodToDisplay], 'days');
     this.timeInterval.start = start;
     this.timeInterval.end = today;
+    this.applyFilters();
     this.kpiService.timeInterval$$.next(this.timeInterval);
   }
 
@@ -66,7 +82,11 @@ export class CommandBarComponent {
     );
   }
 
-  constructor(private activatedRoute: ActivatedRoute) {
+  constructor(private activatedRoute: ActivatedRoute, public observer: BreakpointObserver) {
+    this.observer.observe(['(max-width: 1600px)']).subscribe((res) => {
+      this.granularityType = res.matches ? 'select' : 'button';
+    });
+
     this.activatedRoute.queryParams.subscribe(params => {
       const timeInterval: Partial<TimeInterval> = {
         start: params['start'] ? moment(params['start']) : undefined,
