11994977a73624ff2fa33d486f1e7abff9290cc2
Tobias loch <Tobias.loch@mail.de>
2023-12-17 13:53:08 +0100

    layouting fix for overview and comparision page (are now more responsive for smaller screen sizes) added title of comparision view changed navbar name of comparision view

--- src/app/components/navigation/navbar.component.ts
+++ src/app/components/navigation/navbar.component.ts
@@ -14,7 +14,7 @@ export class NavbarComponent {
   public menuItems: MenuItem[] = [
     { name: 'Overview', icon: 'gallery_thumbnail', route: '/overview' },
     { name: 'CO₂ Emissions', icon: 'bubble_chart', route: '/co2emissions' },
-    { name: 'CO₂ Emissions Comparison', icon: 'compare_arrows', route: '/comparison' },
+    { name: 'CO₂ Emissions Compared', icon: 'compare_arrows', route: '/comparison' },
     { name: 'Reporting', icon: 'assignment', route: '/reporting' },
     { name: 'Datasets', icon: 'dataset', route: '/datasets' },
     { name: 'Energy Flow', icon: 'timeline', route: '/energyflow' },
