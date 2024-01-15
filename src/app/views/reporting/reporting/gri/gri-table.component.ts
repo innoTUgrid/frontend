5633cba758e355e3fbcb352f6236d2f9107a6b1a
Tobias loch <Tobias.loch@mail.de>
2024-01-15 15:12:53 +0100

    added comments which describe the source of the data

--- src/app/views/reporting/reporting/gri/gri-table.component.ts
+++ src/app/views/reporting/reporting/gri/gri-table.component.ts
@@ -35,36 +35,37 @@ export interface Element {
 const ELEMENT_DATA: Element[] = [
 
     // GRI 302-1
-    {gri_modul: "GRI 302-1 Energy consumption within the organization", description: "a. Total fuel consumption within the organization from non-renewable sources including fuel types used", unit: "-", year_2023:"-", year_2022:"-"},
-    {gri_modul: "", description: "b. Total fuel consumption within the organization from renewable sources including fuel types used", unit:"-", year_2023:"-", year_2022:"-"},
-    {gri_modul: "", description: "c. i. Electricity consumption", unit:"-", year_2023:"-", year_2022:"-"},
-    {gri_modul: "", description: "c. ii. Heating consumption", unit:"-", year_2023:"-", year_2022:"-"},
-    {gri_modul: "", description: "c. iii. Cooling consumption", unit:"-", year_2023:"-", year_2022:"-"},
-    {gri_modul: "", description: "c. iv. Steam consumption", unit:"-", year_2023:"-", year_2022:"-"},
-    {gri_modul: "", description: "d. i. Electricity sold", unit:"-", year_2023:"-", year_2022:"-"},
-    {gri_modul: "", description: "d. ii. Heating sold", unit:"-", year_2023:"-", year_2022:"-"},
-    {gri_modul: "", description: "d. iii. Cooling sold", unit:"-", year_2023:"-", year_2022:"-"},
-    {gri_modul: "", description: "d. iv. Steam sold", unit:"-", year_2023:"-", year_2022:"-"},
-    {gri_modul: "", description: "e. Total energy consumption within the organization", unit:"-", year_2023:"-", year_2022:"-"},
-    {gri_modul: "", description: "f. Standards, methodologies, assumptions, and/or calculation tools used", unit:"-", year_2023:"-", year_2022:"-"},
-    {gri_modul: "", description: "g. Source of the conversion factors used", unit:"-", year_2023:"-", year_2022:"-"},
+    {gri_modul: "GRI 302-1 Energy consumption within the organization", description: "a. Total fuel consumption within the organization from non-renewable sources including fuel types used", unit: "-", year_2023:"-", year_2022:"-"}, // no data for this
+    {gri_modul: "", description: "b. Total fuel consumption within the organization from renewable sources including fuel types used", unit:"-", year_2023:"-", year_2022:"-"}, // Biogas kwh (consumption endpoint)
+    {gri_modul: "", description: "c. i. Electricity consumption", unit:"-", year_2023:"-", year_2022:"-"}, // total consumption (consumption endpoint)
+    {gri_modul: "", description: "c. ii. Heating consumption", unit:"-", year_2023:"-", year_2022:"-"}, // leer
+    {gri_modul: "", description: "c. iii. Cooling consumption", unit:"-", year_2023:"-", year_2022:"-"}, // leer
+    {gri_modul: "", description: "c. iv. Steam consumption", unit:"-", year_2023:"-", year_2022:"-"}, // leer
+    {gri_modul: "", description: "d. i. Electricity sold", unit:"-", year_2023:"-", year_2022:"-"}, // leer
+    {gri_modul: "", description: "d. ii. Heating sold", unit:"-", year_2023:"-", year_2022:"-"}, // leer
+    {gri_modul: "", description: "d. iii. Cooling sold", unit:"-", year_2023:"-", year_2022:"-"}, // leer
+    {gri_modul: "", description: "d. iv. Steam sold", unit:"-", year_2023:"-", year_2022:"-"}, // leer
+    {gri_modul: "", description: "e. Total energy consumption within the organization", unit:"-", year_2023:"-", year_2022:"-"}, // leer
+    {gri_modul: "", description: "f. Standards, methodologies, assumptions, and/or calculation tools used", unit:"-", year_2023:"-", year_2022:"-"}, // fulltext TODO
+    {gri_modul: "", description: "g. Source of the conversion factors used", unit:"-", year_2023:"-", year_2022:"-"}, // leer
 
     // GRI 305-1
-    {gri_modul: "GRI 305-1 Direct (Scope 1) GHG emissions", description: "a. Gross direct (Scope 1) GHG emissions", unit:"-", year_2023:"-", year_2022:"-"},
-    {gri_modul: "", description: "c. Biogenic CO2 emissions ", unit:"-", year_2023:"-", year_2022:"-"},
-    {gri_modul: "", description: "d. Base year for the calculation, if applicable", unit:"-", year_2023:"-", year_2022:"-"},
-    {gri_modul: "", description: "e. Source of the emission factors and the global warming potential (GWP) rates used, or a reference to the GWP source", unit:"-", year_2023:"-", year_2022:"-"},
-    {gri_modul: "", description: "f. Consolidation approach for emissions; whether equity share, financial control, or operational control", unit:"-", year_2023:"-", year_2022:"-"},
-    {gri_modul: "", description: "g. Standards, methodologies, assumptions, and/or calculation tools used", unit:"-", year_2023:"-", year_2022:"-"},
+    {gri_modul: "GRI 305-1 Direct (Scope 1) GHG emissions", description: "a. Gross direct (Scope 1) GHG emissions", unit:"-", year_2023:"-", year_2022:"-"}, // total scope 1 emissions 
+    {gri_modul: "", description: "b. Gases included in the calculation", unit:"-", year_2023:"-", year_2022:"-"}, // fixed value 
+    {gri_modul: "", description: "c. Biogenic CO2 emissions ", unit:"-", year_2023:"-", year_2022:"-"}, // emissions of biogas
+    {gri_modul: "", description: "d. Base year for the calculation, if applicable", unit:"-", year_2023:"-", year_2022:"-"}, // leer
+    {gri_modul: "", description: "e. Source of the emission factors and the global warming potential (GWP) rates used, or a reference to the GWP source", unit:"-", year_2023:"-", year_2022:"-"}, // source of emission factors, not yet implemented
+    {gri_modul: "", description: "f. Consolidation approach for emissions; whether equity share, financial control, or operational control", unit:"-", year_2023:"-", year_2022:"-"}, // leer
+    {gri_modul: "", description: "g. Standards, methodologies, assumptions, and/or calculation tools used", unit:"-", year_2023:"-", year_2022:"-"}, // fulltext TODO
 
     // GRI 305-2
-    {gri_modul: "GRI 305-2 Energy indirect (Scope 2) GHG emissions", description: "a. Gross location-based energy indirect (Scope 2) GHG emissions", unit:"-", year_2023:"-", year_2022:"-"},
-    {gri_modul: "", description: "b. If applicable, gross market-based energy indirect (Scope 2) GHG emissions", unit:"-", year_2023:"-", year_2022:"-"},
-    {gri_modul: "", description: "c. Gases included in the calculation", unit:"-", year_2023:"-", year_2022:"-"},
-    {gri_modul: "", description: "d. Base year for the calculation, if applicable", unit:"-", year_2023:"-", year_2022:"-"},
-    {gri_modul: "", description: "e. Source of the emission factors and the global warming potential (GWP) rates used, or a reference to the GWP source", unit:"-", year_2023:"-", year_2022:"-"},
-    {gri_modul: "", description: "f. Consolidation approach for emissions; whether equity share, financial control, or operational control", unit:"-", year_2023:"-", year_2022:"-"},
-    {gri_modul: "", description: "g. Standards, methodologies, assumptions, and/or calculation tools used", unit:"-", year_2023:"-", year_2022:"-"},
+    {gri_modul: "GRI 305-2 Energy indirect (Scope 2) GHG emissions", description: "a. Gross location-based energy indirect (Scope 2) GHG emissions", unit:"-", year_2023:"-", year_2022:"-"}, // total scope 2 emissions
+    {gri_modul: "", description: "b. If applicable, gross market-based energy indirect (Scope 2) GHG emissions", unit:"-", year_2023:"-", year_2022:"-"}, // leer
+    {gri_modul: "", description: "c. Gases included in the calculation", unit:"-", year_2023:"-", year_2022:"-"}, // fixed value co2
+    {gri_modul: "", description: "d. Base year for the calculation, if applicable", unit:"-", year_2023:"-", year_2022:"-"}, // leer
+    {gri_modul: "", description: "e. Source of the emission factors and the global warming potential (GWP) rates used, or a reference to the GWP source", unit:"-", year_2023:"-", year_2022:"-"}, // source of emission factors, not yet implemented
+    {gri_modul: "", description: "f. Consolidation approach for emissions; whether equity share, financial control, or operational control", unit:"-", year_2023:"-", year_2022:"-"}, // leer
+    {gri_modul: "", description: "g. Standards, methodologies, assumptions, and/or calculation tools used", unit:"-", year_2023:"-", year_2022:"-"}, // fulltext TODO
 
 ];
 
