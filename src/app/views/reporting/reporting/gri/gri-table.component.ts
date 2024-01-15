67f4517444f732b789fc0a95917a60b756d7ddbb
Tobias loch <Tobias.loch@mail.de>
2024-01-15 17:46:44 +0100

    in gri report: - instead of using angular material table we use now mtx data grid 	for simplicity reasons

--- src/app/views/reporting/reporting/gri/gri-table.component.ts
+++ src/app/views/reporting/reporting/gri/gri-table.component.ts
@@ -1,6 +1,26 @@
-import {Component,ViewChild, ElementRef} from '@angular/core';
+import {Component,ViewChild, ElementRef, Input} from '@angular/core';
+import { MtxGridColumn } from '@ng-matero/extensions/grid';
 import * as XLSX from 'xlsx';
 
+class GRIElement {
+  gri_modul: string;
+  description: string;
+  unit: string;
+  year_first: string;
+  year_second: string;
+
+  // a function that takes a year and returns the value for that year
+  data_loader?: (year: number) => number;
+
+  constructor(gri_modul: string, description: string, unit: string, year_2023: string, year_2022: string, data_loader?: (year: number) => number) {
+    this.gri_modul = gri_modul;
+    this.description = description;
+    this.unit = unit;
+    this.year_first = year_2023;
+    this.year_second = year_2022;
+    this.data_loader = data_loader;
+  }
+}
 
 @Component({
   selector: 'gri-table',
@@ -10,62 +30,67 @@ import * as XLSX from 'xlsx';
 
 export class TableBasicExample {
   @ViewChild('TABLE') table!: ElementRef;
-  displayedColumns = ['gri_modul', 'description', 'unit', 'year_2023', 'year_2022'];
-  dataSource = ELEMENT_DATA;
 
-// Excel export function
-ExportTOExcel()
-{
-  const ws: XLSX.WorkSheet=XLSX.utils.table_to_sheet(this.table.nativeElement);
-  const wb: XLSX.WorkBook = XLSX.utils.book_new();
-  XLSX.utils.book_append_sheet(wb, ws, 'GRI_Report');
-  
-  XLSX.writeFile(wb, 'GRI_Report.xlsx');
-  
-}}
+  @Input() title: string = '';
 
-export interface Element {
-    gri_modul: string;
-    description: string;
-    unit: string;
-    year_2023: string;
-    year_2022: string;
+  // Excel export function
+  ExportTOExcel()
+  {
+    const ws: XLSX.WorkSheet=XLSX.utils.table_to_sheet(this.table.nativeElement);
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'GRI_Report');
+    
+    XLSX.writeFile(wb, 'GRI_Report.xlsx');
+    
+  }
+
+  columns: MtxGridColumn[] = [
+    { header: 'GRI Modul', field: 'gri_modul', width: '30%' },
+    { header: 'Description', field: 'description', width: '40%' },
+    { header: 'Unit', field: 'unit', width: '10%' },
+    { header: '2023', field: 'year_first', width: '10%' },
+    { header: '2022', field: 'year_second', width: '10%' },
+  ]
+
+  list = ELEMENT_DATA
 }
 
-const ELEMENT_DATA: Element[] = [
+
+
+const ELEMENT_DATA: GRIElement[] = [
 
     // GRI 302-1
-    {gri_modul: "GRI 302-1 Energy consumption within the organization", description: "a. Total fuel consumption within the organization from non-renewable sources including fuel types used", unit: "-", year_2023:"-", year_2022:"-"}, // no data for this
-    {gri_modul: "", description: "b. Total fuel consumption within the organization from renewable sources including fuel types used", unit:"-", year_2023:"-", year_2022:"-"}, // Biogas kwh (consumption endpoint)
-    {gri_modul: "", description: "c. i. Electricity consumption", unit:"-", year_2023:"-", year_2022:"-"}, // total consumption (consumption endpoint)
-    {gri_modul: "", description: "c. ii. Heating consumption", unit:"-", year_2023:"-", year_2022:"-"}, // leer
-    {gri_modul: "", description: "c. iii. Cooling consumption", unit:"-", year_2023:"-", year_2022:"-"}, // leer
-    {gri_modul: "", description: "c. iv. Steam consumption", unit:"-", year_2023:"-", year_2022:"-"}, // leer
-    {gri_modul: "", description: "d. i. Electricity sold", unit:"-", year_2023:"-", year_2022:"-"}, // leer
-    {gri_modul: "", description: "d. ii. Heating sold", unit:"-", year_2023:"-", year_2022:"-"}, // leer
-    {gri_modul: "", description: "d. iii. Cooling sold", unit:"-", year_2023:"-", year_2022:"-"}, // leer
-    {gri_modul: "", description: "d. iv. Steam sold", unit:"-", year_2023:"-", year_2022:"-"}, // leer
-    {gri_modul: "", description: "e. Total energy consumption within the organization", unit:"-", year_2023:"-", year_2022:"-"}, // leer
-    {gri_modul: "", description: "f. Standards, methodologies, assumptions, and/or calculation tools used", unit:"-", year_2023:"-", year_2022:"-"}, // fulltext TODO
-    {gri_modul: "", description: "g. Source of the conversion factors used", unit:"-", year_2023:"-", year_2022:"-"}, // leer
+    {gri_modul: "GRI 302-1 Energy consumption within the organization", description: "a. Total fuel consumption within the organization from non-renewable sources including fuel types used", unit: "-", year_first:"-", year_second:"-"}, // no data for this
+    {gri_modul: "", description: "b. Total fuel consumption within the organization from renewable sources including fuel types used", unit:"-", year_first:"-", year_second:"-"}, // Biogas kwh (consumption endpoint)
+    {gri_modul: "", description: "c. i. Electricity consumption", unit:"-", year_first:"-", year_second:"-"}, // total consumption (consumption endpoint)
+    {gri_modul: "", description: "c. ii. Heating consumption", unit:"-", year_first:"-", year_second:"-"}, // leer
+    {gri_modul: "", description: "c. iii. Cooling consumption", unit:"-", year_first:"-", year_second:"-"}, // leer
+    {gri_modul: "", description: "c. iv. Steam consumption", unit:"-", year_first:"-", year_second:"-"}, // leer
+    {gri_modul: "", description: "d. i. Electricity sold", unit:"-", year_first:"-", year_second:"-"}, // leer
+    {gri_modul: "", description: "d. ii. Heating sold", unit:"-", year_first:"-", year_second:"-"}, // leer
+    {gri_modul: "", description: "d. iii. Cooling sold", unit:"-", year_first:"-", year_second:"-"}, // leer
+    {gri_modul: "", description: "d. iv. Steam sold", unit:"-", year_first:"-", year_second:"-"}, // leer
+    {gri_modul: "", description: "e. Total energy consumption within the organization", unit:"-", year_first:"-", year_second:"-"}, // leer
+    {gri_modul: "", description: "f. Standards, methodologies, assumptions, and/or calculation tools used", unit:"-", year_first:"-", year_second:"-"}, // fulltext TODO
+    {gri_modul: "", description: "g. Source of the conversion factors used", unit:"-", year_first:"-", year_second:"-"}, // leer
 
     // GRI 305-1
-    {gri_modul: "GRI 305-1 Direct (Scope 1) GHG emissions", description: "a. Gross direct (Scope 1) GHG emissions", unit:"-", year_2023:"-", year_2022:"-"}, // total scope 1 emissions 
-    {gri_modul: "", description: "b. Gases included in the calculation", unit:"-", year_2023:"-", year_2022:"-"}, // fixed value 
-    {gri_modul: "", description: "c. Biogenic CO2 emissions ", unit:"-", year_2023:"-", year_2022:"-"}, // emissions of biogas
-    {gri_modul: "", description: "d. Base year for the calculation, if applicable", unit:"-", year_2023:"-", year_2022:"-"}, // leer
-    {gri_modul: "", description: "e. Source of the emission factors and the global warming potential (GWP) rates used, or a reference to the GWP source", unit:"-", year_2023:"-", year_2022:"-"}, // source of emission factors, not yet implemented
-    {gri_modul: "", description: "f. Consolidation approach for emissions; whether equity share, financial control, or operational control", unit:"-", year_2023:"-", year_2022:"-"}, // leer
-    {gri_modul: "", description: "g. Standards, methodologies, assumptions, and/or calculation tools used", unit:"-", year_2023:"-", year_2022:"-"}, // fulltext TODO
+    {gri_modul: "GRI 305-1 Direct (Scope 1) GHG emissions", description: "a. Gross direct (Scope 1) GHG emissions", unit:"-", year_first:"-", year_second:"-"}, // total scope 1 emissions 
+    {gri_modul: "", description: "b. Gases included in the calculation", unit:"-", year_first:"-", year_second:"-"}, // fixed value 
+    {gri_modul: "", description: "c. Biogenic CO2 emissions ", unit:"-", year_first:"-", year_second:"-"}, // emissions of biogas
+    {gri_modul: "", description: "d. Base year for the calculation, if applicable", unit:"-", year_first:"-", year_second:"-"}, // leer
+    {gri_modul: "", description: "e. Source of the emission factors and the global warming potential (GWP) rates used, or a reference to the GWP source", unit:"-", year_first:"-", year_second:"-"}, // source of emission factors, not yet implemented
+    {gri_modul: "", description: "f. Consolidation approach for emissions; whether equity share, financial control, or operational control", unit:"-", year_first:"-", year_second:"-"}, // leer
+    {gri_modul: "", description: "g. Standards, methodologies, assumptions, and/or calculation tools used", unit:"-", year_first:"-", year_second:"-"}, // fulltext TODO
 
     // GRI 305-2
-    {gri_modul: "GRI 305-2 Energy indirect (Scope 2) GHG emissions", description: "a. Gross location-based energy indirect (Scope 2) GHG emissions", unit:"-", year_2023:"-", year_2022:"-"}, // total scope 2 emissions
-    {gri_modul: "", description: "b. If applicable, gross market-based energy indirect (Scope 2) GHG emissions", unit:"-", year_2023:"-", year_2022:"-"}, // leer
-    {gri_modul: "", description: "c. Gases included in the calculation", unit:"-", year_2023:"-", year_2022:"-"}, // fixed value co2
-    {gri_modul: "", description: "d. Base year for the calculation, if applicable", unit:"-", year_2023:"-", year_2022:"-"}, // leer
-    {gri_modul: "", description: "e. Source of the emission factors and the global warming potential (GWP) rates used, or a reference to the GWP source", unit:"-", year_2023:"-", year_2022:"-"}, // source of emission factors, not yet implemented
-    {gri_modul: "", description: "f. Consolidation approach for emissions; whether equity share, financial control, or operational control", unit:"-", year_2023:"-", year_2022:"-"}, // leer
-    {gri_modul: "", description: "g. Standards, methodologies, assumptions, and/or calculation tools used", unit:"-", year_2023:"-", year_2022:"-"}, // fulltext TODO
+    {gri_modul: "GRI 305-2 Energy indirect (Scope 2) GHG emissions", description: "a. Gross location-based energy indirect (Scope 2) GHG emissions", unit:"-", year_first:"-", year_second:"-"}, // total scope 2 emissions
+    {gri_modul: "", description: "b. If applicable, gross market-based energy indirect (Scope 2) GHG emissions", unit:"-", year_first:"-", year_second:"-"}, // leer
+    {gri_modul: "", description: "c. Gases included in the calculation", unit:"-", year_first:"-", year_second:"-"}, // fixed value co2
+    {gri_modul: "", description: "d. Base year for the calculation, if applicable", unit:"-", year_first:"-", year_second:"-"}, // leer
+    {gri_modul: "", description: "e. Source of the emission factors and the global warming potential (GWP) rates used, or a reference to the GWP source", unit:"-", year_first:"-", year_second:"-"}, // source of emission factors, not yet implemented
+    {gri_modul: "", description: "f. Consolidation approach for emissions; whether equity share, financial control, or operational control", unit:"-", year_first:"-", year_second:"-"}, // leer
+    {gri_modul: "", description: "g. Standards, methodologies, assumptions, and/or calculation tools used", unit:"-", year_first:"-", year_second:"-"}, // fulltext TODO
 
 ];
 
