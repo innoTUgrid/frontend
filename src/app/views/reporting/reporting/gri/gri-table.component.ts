334d749c7975c0945d722978ef3a5e5022a9b68c
Tobias loch <Tobias.loch@mail.de>
2024-01-15 19:33:11 +0100

    first version of data ingestion somehow the data is fetched twice on load

--- src/app/views/reporting/reporting/gri/gri-table.component.ts
+++ src/app/views/reporting/reporting/gri/gri-table.component.ts
@@ -1,25 +1,23 @@
-import {Component,ViewChild, ElementRef, Input} from '@angular/core';
+import {Component,ViewChild, ElementRef, Input, inject} from '@angular/core';
+import { ChartService } from '@app/services/chart.service';
+import { DataService } from '@app/services/data.service';
+import { DatasetKey, TimeSeriesEndpointKey } from '@app/types/kpi.model';
+import { DatasetRegistry, TimeInterval } from '@app/types/time-series-data.model';
 import { MtxGridColumn } from '@ng-matero/extensions/grid';
+import { Subscription, combineLatest } from 'rxjs';
 import * as XLSX from 'xlsx';
 
-class GRIElement {
+enum DataTypes {
+  BIOGAS = 'biogas',
+}
+
+export interface Element {
   gri_modul: string;
   description: string;
   unit: string;
   year_first: string;
   year_second: string;
-
-  // a function that takes a year and returns the value for that year
-  data_loader?: (year: number) => number;
-
-  constructor(gri_modul: string, description: string, unit: string, year_2023: string, year_2022: string, data_loader?: (year: number) => number) {
-    this.gri_modul = gri_modul;
-    this.description = description;
-    this.unit = unit;
-    this.year_first = year_2023;
-    this.year_second = year_2022;
-    this.data_loader = data_loader;
-  }
+  data_loader?: (interval:TimeInterval) => string;
 }
 
 @Component({
@@ -30,8 +28,41 @@ class GRIElement {
 
 export class TableBasicExample {
   @ViewChild('TABLE') table!: ElementRef;
+  dataService: DataService = inject(DataService);
+  chartService: ChartService = inject(ChartService);
 
   @Input() title: string = '';
+
+  id = "GRI." + Math.random().toString(36).substring(7);
+
+  registries: DatasetRegistry[] = [
+    {id:this.id, endpointKey: TimeSeriesEndpointKey.ENERGY_CONSUMPTION},
+    {id:this.id, endpointKey: TimeSeriesEndpointKey.SCOPE_2_EMISSIONS},
+  ]
+
+  subscriptions: Subscription[] = [];
+
+  ngOnInit() {
+    this.registries.forEach((registry) => {
+      this.dataService.registerDataset(registry)
+    })
+
+    const s = combineLatest([this.dataService.timeInterval.getValue(), ...this.registries.map((registry) => this.dataService.getDataset(registry.endpointKey))])
+    .subscribe((datasets) => {
+      const timeIntervals = this.dataService.timeInterval.getValue()
+      console.log(timeIntervals)
+      this.updateData(timeIntervals)
+    })
+    this.subscriptions.push(s)
+  }
+
+  ngOnDestroy() {
+    this.registries.forEach((registry) => {
+      this.dataService.unregisterDataset(registry)
+    })
+
+    this.subscriptions.forEach((subscription) => {subscription.unsubscribe()})
+  }
 
   // Excel export function
   ExportTOExcel()
@@ -52,17 +83,37 @@ export class TableBasicExample {
     { header: '2022', field: 'year_second', width: '10%' },
   ]
 
-  list = ELEMENT_DATA
-}
+  
+  updateData(timeIntervals: TimeInterval[]) {
+    if (timeIntervals.length < 2) {
+      console.error("GRI table needs two time intervals, but got only " + timeIntervals.length + ".")
+      return;
+    }
+    for (const element of this.list) {
+      if (element.data_loader) {
+        element.year_first = element.data_loader(timeIntervals[0])
+        element.year_second = element.data_loader(timeIntervals[1])
+      }
+    }
+    this.list = [...this.list]
+  }
 
+  calculateTotal(datasetKey: DatasetKey, types?: string[]): (interval: TimeInterval) => string {
+    return (timeInterval: TimeInterval) => {
+      const dataset = this.dataService.getDataset(datasetKey).getValue()
+      const series = dataset.series.filter((s) => types ? types.includes(s.type) : true)
+      const dataSummed = this.chartService.sumAllDataTypes(series, timeInterval)
+      const total = this.chartService.calculateSingleValue(dataSummed, false)
+      return ChartService.addThousandsSeparator(total?.toFixed(2).replace('.', ',')) ?? '-'
+    }
+  }
 
-
-const ELEMENT_DATA: GRIElement[] = [
+  list: Element[] = [
 
     // GRI 302-1
     {gri_modul: "GRI 302-1 Energy consumption within the organization", description: "a. Total fuel consumption within the organization from non-renewable sources including fuel types used", unit: "-", year_first:"-", year_second:"-"}, // no data for this
-    {gri_modul: "", description: "b. Total fuel consumption within the organization from renewable sources including fuel types used", unit:"-", year_first:"-", year_second:"-"}, // Biogas kwh (consumption endpoint)
-    {gri_modul: "", description: "c. i. Electricity consumption", unit:"-", year_first:"-", year_second:"-"}, // total consumption (consumption endpoint)
+    {gri_modul: "", description: "b. Total fuel consumption within the organization from renewable sources including fuel types used", unit:"-", year_first:"-", year_second:"-", data_loader: this.calculateTotal(TimeSeriesEndpointKey.ENERGY_CONSUMPTION, [DataTypes.BIOGAS])}, // Biogas kwh (consumption endpoint)
+    {gri_modul: "", description: "c. i. Electricity consumption", unit:"-", year_first:"-", year_second:"-", data_loader: this.calculateTotal(TimeSeriesEndpointKey.ENERGY_CONSUMPTION)}, // total consumption (consumption endpoint)
     {gri_modul: "", description: "c. ii. Heating consumption", unit:"-", year_first:"-", year_second:"-"}, // leer
     {gri_modul: "", description: "c. iii. Cooling consumption", unit:"-", year_first:"-", year_second:"-"}, // leer
     {gri_modul: "", description: "c. iv. Steam consumption", unit:"-", year_first:"-", year_second:"-"}, // leer
@@ -77,14 +128,14 @@ const ELEMENT_DATA: GRIElement[] = [
     // GRI 305-1
     {gri_modul: "GRI 305-1 Direct (Scope 1) GHG emissions", description: "a. Gross direct (Scope 1) GHG emissions", unit:"-", year_first:"-", year_second:"-"}, // total scope 1 emissions 
     {gri_modul: "", description: "b. Gases included in the calculation", unit:"-", year_first:"-", year_second:"-"}, // fixed value 
-    {gri_modul: "", description: "c. Biogenic CO2 emissions ", unit:"-", year_first:"-", year_second:"-"}, // emissions of biogas
+    {gri_modul: "", description: "c. Biogenic CO2 emissions ", unit:"-", year_first:"-", year_second:"-", data_loader:this.calculateTotal(TimeSeriesEndpointKey.SCOPE_2_EMISSIONS, [DataTypes.BIOGAS])}, // emissions of biogas
     {gri_modul: "", description: "d. Base year for the calculation, if applicable", unit:"-", year_first:"-", year_second:"-"}, // leer
     {gri_modul: "", description: "e. Source of the emission factors and the global warming potential (GWP) rates used, or a reference to the GWP source", unit:"-", year_first:"-", year_second:"-"}, // source of emission factors, not yet implemented
     {gri_modul: "", description: "f. Consolidation approach for emissions; whether equity share, financial control, or operational control", unit:"-", year_first:"-", year_second:"-"}, // leer
     {gri_modul: "", description: "g. Standards, methodologies, assumptions, and/or calculation tools used", unit:"-", year_first:"-", year_second:"-"}, // fulltext TODO
 
     // GRI 305-2
-    {gri_modul: "GRI 305-2 Energy indirect (Scope 2) GHG emissions", description: "a. Gross location-based energy indirect (Scope 2) GHG emissions", unit:"-", year_first:"-", year_second:"-"}, // total scope 2 emissions
+    {gri_modul: "GRI 305-2 Energy indirect (Scope 2) GHG emissions", description: "a. Gross location-based energy indirect (Scope 2) GHG emissions", unit:"-", year_first:"-", year_second:"-", data_loader:this.calculateTotal(TimeSeriesEndpointKey.SCOPE_2_EMISSIONS)}, // total scope 2 emissions
     {gri_modul: "", description: "b. If applicable, gross market-based energy indirect (Scope 2) GHG emissions", unit:"-", year_first:"-", year_second:"-"}, // leer
     {gri_modul: "", description: "c. Gases included in the calculation", unit:"-", year_first:"-", year_second:"-"}, // fixed value co2
     {gri_modul: "", description: "d. Base year for the calculation, if applicable", unit:"-", year_first:"-", year_second:"-"}, // leer
@@ -93,4 +144,6 @@ const ELEMENT_DATA: GRIElement[] = [
     {gri_modul: "", description: "g. Standards, methodologies, assumptions, and/or calculation tools used", unit:"-", year_first:"-", year_second:"-"}, // fulltext TODO
 
 ];
+}
+
 
